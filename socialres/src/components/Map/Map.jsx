5f3f67080bc93c2576d95f484b25d9829267f21c
gcontreras3 <103069981+gcontreras3@users.noreply.github.com>
2022-09-15 22:11:40 -0500

    made changes

--- socialres/src/components/Map/Map.jsx
+++ socialres/src/components/Map/Map.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
 import GoogleMapReact from 'google-map-react';
 import { useMediaQuery } from '@material-ui/core';
-// import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
-// import Rating from '@material-ui/lab/Rating';
+import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
+import Rating from '@material-ui/lab/Rating';
+
 import useStyles from './styles';
 
 
-const Map = () => {
-  
+const Map = ({ setCoordinates, setBounds, coordinates}) => {
+  const classes = useStyles();
+  const isMobile = useMediaQuery('(min-width:600px)');
+
   
   return (
-    <div>
-      
-        <h1>Map</h1>
+    <div className={classes.mapContainer}>
+      <GoogleMapReact
+          bootstrapURLKeys={{ key: 'AIzaSyAXnEqSl2niWZadItWOjU-N8VaBxu9yHR8' }}
+          defaultCenter={coordinates}
+          center={coordinates}
+          defaultZoom={14}
+          margin={[50, 50, 50, 50]}
+          option={''}
+          onChange={(e) => {
+            
+              setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+              setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+          }}
+          onChildClick={''}
+      >
+
+      </GoogleMapReact>
      
     </div>
   );
